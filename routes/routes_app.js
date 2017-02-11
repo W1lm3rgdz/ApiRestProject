0764affe2f18af388219e2f6602fe55323f06b3e
Wilmer Guedez <wilmer.guedz@gmail.com>
2017-02-11 01:29:12 -0700

    modify routes for user

--- routes/routes_app.js
+++ routes/routes_app.js
@@ -11,11 +11,31 @@
 //----------------------------------------------------------------------------//
 
 //Load packages required
-var express    = require('express');
+var express          = require('express');
+var userController   = require('../controllers/user');
 
 //Create Express Router
 var router = express.Router();
 
+//User
+router.route('/user/addUser')
+.post(userController.addUser);
+//.get(authController.isAuthenticated, userController.addUser);
 
+router.route('/user/searchAllUser')
+.get(userController.searchAllUser);
+//.get(authController.isAuthenticated, userController.searchAllUser);
+
+router.route('/user/searchByIdUser/:id')
+.get(userController.searchByIdUser);
+//.get(authController.isAuthenticated, userController.searchByIdUser);
+
+router.route('/user/updateUser/:id')
+.put(userController.updateUser);
+//.put(authController.isAuthenticated, userController.updateUser);
+
+router.route('/user/removeUser/:id')
+.delete(userController.removeUser);
+//.delete(authController.isAuthenticated, userController.removeUser);
 
 module.exports = router;
