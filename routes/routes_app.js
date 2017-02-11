129bfc8a34e5ecdd16e835e5a28dced5525add76
Wilmer Guedez <wilmer.guedz@gmail.com>
2017-02-11 01:59:02 -0700

    update of modular routes user and add oauthcontroller.js

--- routes/routes_app.js
+++ routes/routes_app.js
@@ -13,29 +13,25 @@
 //Load packages required
 var express          = require('express');
 var userController   = require('../controllers/user');
+var authController   = require('../controllers/oauthController');
 
 //Create Express Router
 var router = express.Router();
 
 //User
 router.route('/user/addUser')
-.post(userController.addUser);
-//.get(authController.isAuthenticated, userController.addUser);
+.get(authController.isAuthenticated, userController.addUser);
 
 router.route('/user/searchAllUser')
-.get(userController.searchAllUser);
-//.get(authController.isAuthenticated, userController.searchAllUser);
+.get(authController.isAuthenticated, userController.searchAllUser);
 
 router.route('/user/searchByIdUser/:id')
-.get(userController.searchByIdUser);
-//.get(authController.isAuthenticated, userController.searchByIdUser);
+.get(authController.isAuthenticated, userController.searchByIdUser);
 
 router.route('/user/updateUser/:id')
-.put(userController.updateUser);
-//.put(authController.isAuthenticated, userController.updateUser);
+.put(authController.isAuthenticated, userController.updateUser);
 
 router.route('/user/removeUser/:id')
-.delete(userController.removeUser);
-//.delete(authController.isAuthenticated, userController.removeUser);
+.delete(authController.isAuthenticated, userController.removeUser);
 
 module.exports = router;
