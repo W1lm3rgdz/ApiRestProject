8ec1508a090a2d9b71ce0252ca665d5036ff52bd
Wilmer Guedez <wilmer.guedz@gmail.com>
2017-02-10 23:46:52 -0700

    Load packages required for node.js

--- server/server.js
+++ server/server.js
@@ -7,11 +7,14 @@
 //Fecha: 11/02/2017
 //Autor: Ing. Wilmer Guedez
 //Email: wilmer.guedz@gmail.com
-//Descripcion: Api Rest para el control y Seguimiento de Proyectos 
+//Descripcion: Api Rest for the control of projects and requirements
 //----------------------------------------------------------------------------//
 
-//importando paquetes necesarios
-var express = require('express');
+//Load packages required
+var express    = require('express');
+var mongoose   = require('mongoose');
+var bodyParser = require('body-parser'); 
+var passport   = require('passport');
 
 var app = express();
 
