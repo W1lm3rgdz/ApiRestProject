b740aa20974eb7cffa333e7a174f905149976975
Wilmer Guedez <wilmer.guedz@gmail.com>
2017-02-10 23:59:36 -0700

    Definition of global variables

--- server/server.js
+++ server/server.js
@@ -16,12 +16,16 @@ var mongoose   = require('mongoose');
 var bodyParser = require('body-parser'); 
 var passport   = require('passport');
 
-var app = express();
+//Definition of global variables
+var app = express(); //we create express aplication	
+var port = process.env.port || 3000; //use port 3000 by default 
+app.use(bodyParser.json()); //Use the body-parser package in our application
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/',function(req, res){
-	
-	res.send('hola mundo');
+//Test dummy hello world
+app.get('/',function(req, res){	
+	res.send('Helo World');
 });
 
-app.listen(8080);
+app.listen(port);
 
